4cfece09a44b70b18752566082ea5108ec173083
Mike3247 <72448458+Mike3247@users.noreply.github.com>
2024-09-04 01:05:44 -0400

    refactor

--- script.js
+++ script.js
@@ -1,57 +1,61 @@
 console.log("Hello World!");
 
-const computerPlay = ["rock", "paper", "scissors"];
-const random = Math.floor(Math.random() * computerPlay.length);
-let computerSelection = (random, computerPlay[random]);
-console.log(random, computerPlay[random]);
-console.log(computerSelection);
+function getComputerChoice () {
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return "rock"
+    case 1:
+      return "paper"
+    case 2:
+      return "scissors"
+  }
+}
 
-let playerSelection = prompt("Do you choose rock, paper, or scissors?").toLowerCase();
+function getHumanChoice () { 
+  let humanChoice = prompt("Do you choose rock, paper, or scissors?").toLowerCase()
+  return humanChoice
+};
+
+let humanScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+
+function playRound (humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    roundWinner = 'tie'
+    alert("tie")
+  }
+  if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper') ||
+    (humanChoice === 'paper' && computerChoice === 'rock')
+  ) {
+    humanScore++
+    roundWinner = 'human'
+    alert("human wins this round")
+  }
+  if (
+    (computerChoice === 'rock' && humanChoice === 'scissors') ||
+    (computerChoice === 'scissors' && humanChoice === 'paper') ||
+    (computerChoice === 'paper' && humanChoice === 'rock')
+  ) {
+    computerScore++
+    roundWinner = 'computer'
+    alert("computer wins this round")
+  }
+  // your code here!
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+console.log(computerChoice);
+console.log(humanChoice);
+
+
+playRound(humanChoice, computerChoice);
 
 
 
-/* playerSelection win scenarios: */
-if ((playerSelection === "rock") && (computerSelection === "scissors"))
-    { 
-      alert("Awesome! Rock beat Scissors! You won this round!");
-    }
-  else if((playerSelection === "paper") && (computerSelection === "rock"))
-    {
-      alert("Awesome! Paper beats Rock! You won this round!");
-    }
-  else if ((playerSelection === "scissors") && (computerSelection === "paper"))
-    {
-      alert("Awesome! Scissors beat Paper! You won this round!");
-    }
 
-  /* computerSelection win scenarios: */
-
-
-  else if ((playerSelection === "rock") && (computerSelection === "paper"))
-    { 
-      alert("Nice try! Paper beats Rock! You lost this one! Better luck next time!");
-    }
-  else if((playerSelection === "paper") && (computerSelection === "scissors"))
-    {
-      alert("Nice try! Scissors beat Paper! You lost this one! Better luck next time!");
-    }
-  else if ((playerSelection === "scissors") && (computerSelection === "rock"))
-    {
-      alert("Nice try! Rock beats Scissors! You lost this one! Better luck next time!");
-    }
-
-/* draw scenarios: */
-
-  else if ((playerSelection === "rock") && (computerSelection === "rock"))
-    { 
-      alert("Draw! Rock draws with Rock! Better luck next time!");
-    }
-  else if((playerSelection === "paper") && (computerSelection === "paper"))
-    {
-      alert("Draw! Paper draws with Paper! Better luck next time!");
-    }
-  else if ((playerSelection === "scissors") && (computerSelection === "scissors"))
-    {
-      alert("Draw! Scissors draw with Scissors! Better luck next time!");
-    }
-;
